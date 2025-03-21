dcacc9f937c0b27ac5f509b247294da604e8f54b
ToothlessHaveBun <seventeen@ohdragonboi.cn>
2025-03-21 12:55:54 +0800

    Creat site: use Next.js

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	experimental: {
+		mdxRs: true,
+	},
+	images: {
+		domains: ["evan.beee.top"],
+	}
+};
+
+export default withContentlayer(nextConfig);
